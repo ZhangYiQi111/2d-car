563bcd2da08d618894686ebdaeaf7e518ebc8831
ZhangYiQi111 <son52015021@gmail.com>
2022-12-12 11:52:11 +0800

    Initial commit

---
+++ Assets/sprict/GameManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    GameObject car;
+    GameObject flag;
+    GameObject distance;
+    GameObject Score;
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.car = GameObject.Find("car");
+        this.flag = GameObject.Find("flag");
+        this.distance = GameObject.Find("Distance");
+        this.Score = GameObject.Find("Score");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float length = this.flag.transform.position.x - this.car.transform.position.x;
+        this.distance.GetComponent<Text>().text = "距離目標還有" + length.ToString("F2") + "m";
+        float ScoreNumber = 100 / length;
+        if (ScoreNumber <= 0)
+            ScoreNumber = 0;
+        Score.GetComponent<Text>().text = "分數" + ScoreNumber.ToString("F2") + "分";
+    }
+}
